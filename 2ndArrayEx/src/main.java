2f53c3625ebba8d5aa32ef9e3db88d352ef021b4
YeonMu48 <remembernow48@naver.com>
2022-05-25 15:30:02 +0900

    [추가] 2022-05-18
    - 2차원 배열 연습

---
+++ 2ndArrayEx/src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+	public static void main(String[] args) {
+		int[] arr = new int[3];
+		int[][] arr2 = new int[2][3];
+		
+		// 위와 아래를 비교 
+		
+		int[] arr01 = {1, 2, 3};
+		int[][] arr02 = { {1, 2, 3}, {1, 2, 3}};
+		
+		
+		
+	}
+}
